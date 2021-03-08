550ee34030c5610a959cc372b13ef4a5c8fcf5de
Leonardo <leo1diego2@gmail.com>
2021-03-08 01:15:22 -0600

    first commit

--- src/poo_1/Arreglo.java
+++ src/poo_1/Arreglo.java
@@ -13,30 +13,36 @@ public class Arreglo
 {
    public static void main(String[] args) 
    {  
-    Scanner scan = new Scanner(System.in);
-    int cant,numAux,i,j;
+    Scanner sc = new Scanner(System.in);
+    int num;
+    int i;
+    int j;
+    int n;
+    
     System.out.print("Ingrese la cantidad de números: ");
-    cant = scan.nextInt();
-    int[] numeros = new int[cant + 1];
-    for(i = 1; i <= cant; i++)
+    n = sc.nextInt();
+    
+    int[] numeros = new int[n + 1];
+    
+    for(i = 1; i < n; i++)
     {
-      System.out.print("Ingrese el numero (" + i + ") : ");
-      numeros[i] = scan.nextInt();
+      System.out.print("Ingrese el numero: ");
+      numeros[i] = sc.nextInt();
     }
-    for(i = 1; i <= cant; i++)
+    for(i = 1; i < n; i++)
     {
-      for(j = i; j <= cant; j++)
+      for(j = i; j < n; j++)
     {
     if(numeros[i] < numeros[j])
     {
-      numAux = numeros[i];
+      num = numeros[i];
       numeros[i] = numeros[j];
-      numeros[j] = numAux;
+      numeros[j] = num;
     }
     }
     }
     System.out.println("\nLos números de mayor a menor son:");
-    for(i = 1; i <= cant; i++)
+    for(i = 1; i < n; i++)
       {
         System.out.println(numeros[i]);
       }
