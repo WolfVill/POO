da8dc69b7145e9e3c8b9951f44004571f6f548f6
Leonardo <leo1diego2@gmail.com>
2021-05-17 23:29:26 -0500

    mensaje

---
+++ src/prueba2/Nodo.java
@@ -0,0 +1,32 @@
+package prueba2;
+
+
+
+public class Nodo {
+	Polinomio poli;
+	Nodo siguiente;
+	public Nodo (Polinomio x)
+	{
+		this.poli=x;
+		siguiente = null;
+	}
+
+	public Nodo( Polinomio x, Nodo nuevoelemento){
+
+		this.poli=x;
+		siguiente=nuevoelemento;
+
+	}
+
+	Polinomio getPolinomio()
+	{
+		return this.poli;
+	}
+
+	Nodo getsiguientet()
+	{
+		return siguiente;
+	}
+
+
+}
